5f8164bd2080ea33d8814ca0407cb8ae4f93befd
B. Szonye <bszonye@gmail.com>
2025-02-26 16:18:52 -0800

    fix sticky repair items

---
+++ ui/production-chooser/bz-panel-production-chooser.js
@@ -0,0 +1,17 @@
+export class bzProductionChooserScreen {
+    constructor(panel) {
+        this.panel = panel;
+    }
+
+    beforeAttach() { }
+    afterAttach() {
+        engine.on('ConstructibleChanged', this.panel.onConstructibleAddedToMap, this.panel);
+    }
+    onAttributeChanged(_name, _prev, _next) { }
+    beforeDetach() { }
+    afterDetach() {
+        engine.off('ConstructibleChanged', this.panel.onConstructibleAddedToMap, this.panel);
+    }
+}
+
+Controls.decorate('panel-production-chooser', (val) => new bzProductionChooserScreen(val));
