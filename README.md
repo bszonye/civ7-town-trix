d582dadc3884b923a9715b407f7f58ee0c866489
B. Szonye <bszonye@gmail.com>
2025-03-16 01:55:18 -0700

    City Hall 1.8.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# City Hall 1.8.1
+# City Hall 1.8.2
 better city & town tools for Civilization VII
 
 ## improved production list and city details
--- ui/options/mod-options-decorator.js
+++ ui/options/mod-options-decorator.js
@@ -6,6 +6,21 @@ CategoryData[CategoryType.Mods] = {
     description: "LOC_UI_CONTENT_MGR_SUBTITLE_DESCRIPTION",
 };
 
+// fix Options tab spacing
+const MOD_OPTIONS_STYLE = document.createElement('style');
+MOD_OPTIONS_STYLE.textContent = `
+.option-frame .tab-bar__items .flex {
+    justify-content: space-evenly;
+    align-items: center;
+}
+.option-frame .tab-bar__items .flex-auto {
+    flex: 1 0 auto;
+    min-width: 0rem;
+    margin-left: 0.4444444444rem;
+    margin-right: 0.4444444444rem;
+}`;
+document.head.appendChild(MOD_OPTIONS_STYLE);
+
 // Please, always use ModSettings to save and load settings in your mod.
 // Right now if you try to use **multiple** keys in localStorage, it
 // will break reading from localStorage for **every mod**. This is
